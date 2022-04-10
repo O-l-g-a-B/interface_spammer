79cd3d42a06a525ae78105176202745c6979c0b7
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-04-10 15:47:05 +0300

    first commit

---
+++ styles/index.js
@@ -0,0 +1,20 @@
+const abuse = ['ViAgRA', 'xxx'];
+
+document.querySelector('button').onclick = () => {
+    let text = document.querySelector('textarea').value;
+    for (let i = 0; i < abuse.length; i++) {
+        //abuse[i]
+        while (text.indexOf(abuse[i]) != -1) {
+            text = text.replace(abuse[i], star(abuse[i].length));
+        }
+    }
+    document.querySelector('#out').innerHTML += '<div class="commet">' + text + '</div>';
+}
+
+function star(n) {
+    let out = '';
+    for (let i = 0; i < n; i++) {
+        out += '*';
+    }
+    return out;
+}
